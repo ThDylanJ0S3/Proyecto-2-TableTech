27b2b4b6895561823cc07f7f66fa9a2d97d68412
jeffersonAG <gutierrez22754@gmail.com>
2023-05-14 06:17:02 -0600

    json beta3
    esta versión se corrigió el error de la versión beta 2

--- MasterClientApps/src/controller/MenuPlatillosController.java
+++ MasterClientApps/src/controller/MenuPlatillosController.java
@@ -1,28 +1,18 @@
 package controller;
-
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import modelo.AlertW;
 import modelo.Platillos;
-import org.json.JSONObject;
 
-/**
- * FXML Controller class
- *
- * @author Personal
- */
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
 public class MenuPlatillosController implements Initializable {
 
     // Declaración de objetos y variables de la interfaz gráfica.
@@ -53,19 +43,17 @@ public class MenuPlatillosController implements Initializable {
 
     // Declaración de una lista observable para almacenar objetos Platillos.
     ObservableList<Platillos> platos;
+
     // Declaración de una variable para almacenar la posición del objeto Platillos seleccionado en la tabla.
     private int posicion;
 
-/**
- * Initializes the controller class.
- * Método que se llama después de que se hayan inicializado los objetos de la interfaz gráfica.
- * Este método inicializa la lista observable de Platillos y establece los valores de las propiedades de las columnas.
- */
     /**
      * Initializes the controller class.
-     */    @Override
+     * Método que se llama después de que se hayan inicializado los objetos de la interfaz gráfica.
+     * Este método inicializa la lista observable de Platillos y establece los valores de las propiedades de las columnas.
+     */
+    @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
         // Se inicializa una lista observable de platillos vacía
         platos = FXCollections.observableArrayList();
         // Se configura cada columna de la tabla con la propiedad correspondiente del objeto Platillos
@@ -79,83 +67,83 @@ public class MenuPlatillosController implements Initializable {
     }
 
     @FXML
-    private void agregarPlatillo(ActionEvent event) {
-
+    private void agregarPlatillo() {
         // Se obtienen los valores de los campos de texto correspondientes al nombre del platillo, cantidad de calorías, tiempo de preparación y precio
         String nombrePlatillo = txtNombrePlatillo.getText();
-        int cantidadCalorias = Integer.parseInt(txtCantidadCalorias.getText());
-        int tiempoPreparacion = Integer.parseInt(txtTiempoPreparacion.getText());
-        int precio = Integer.parseInt(txtPrecio.getText());
+        int cantidadCalorias, tiempoPreparacion, precio;
+
+        try {
+            cantidadCalorias = Integer.parseInt(txtCantidadCalorias.getText());
+            tiempoPreparacion = Integer.parseInt(txtTiempoPreparacion.getText());
+            precio = Integer.parseInt(txtPrecio.getText());
+        } catch (NumberFormatException e) {
+            AlertW.display("Error", "Los campos de calorías, tiempo de preparación y precio deben ser números");
+            return;
+        }
 
         // Si alguno de los campos de texto está vacío, se muestra una alerta de error
-        if (txtNombrePlatillo == null || txtCantidadCalorias == null || txtTiempoPreparacion == null || txtPrecio == null) {
+        if (nombrePlatillo.isEmpty() || txtCantidadCalorias.getText().isEmpty() || txtTiempoPreparacion.getText().isEmpty() || txtPrecio.getText().isEmpty()) {
             AlertW.display("Error", "Es necesario llenar todos los espacios");
-        } else {
-            // Si todos los campos de texto tienen valores, se crea un objeto Platillos con esos valores y se agrega a la lista observable de platillos
-            Platillos platillo = new Platillos();
-            platillo.setNombrePlatillo(nombrePlatillo);
-            platillo.setCantCalorias(cantidadCalorias);
-            platillo.setTiempoPreparacion(tiempoPreparacion);
-            platillo.setPrecio(precio);
-            platos.add(platillo);
-
-            // Se crea un objeto JSON con la información del platillo
-            JSONObject platilloJson = new JSONObject();
-            platilloJson.put("nombrePlatillo", nombrePlatillo);
-            platilloJson.put("cantCalorias", cantidadCalorias);
-            platilloJson.put("tiempoPreparacion", tiempoPreparacion);
-            platilloJson.put("precio", precio);
-
-            // Se guarda la información del platillo en un archivo JSON
-            try (FileWriter file = new FileWriter("platillos.json")) {
-                file.write(platilloJson.toString());
-                file.flush();
-                System.out.println("Platillo guardado en platillos.json");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            // Se envía la información del platillo al servidor
-            try {
-                URL url = new URL("./MasterClientApps/src/ServidorSockets/Servidor.java");//cambiar por la url del servidor
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Content-Type", "application/json");
-                conn.setDoOutput(true);
-
-                try (OutputStream os = conn.getOutputStream()) {
-                    byte[] input = platilloJson.toString().getBytes("utf-8");
-                    os.write(input, 0, input.length);
-                }
-
-                try (BufferedReader br = new BufferedReader(
-                        new InputStreamReader(conn.getInputStream(), "utf-8"))) {
-                    StringBuilder response = new StringBuilder();
-                    String responseLine = null;
-                    while ((responseLine = br.readLine()) != null) {
-                        response.append(responseLine.trim());
-                    }
-                    System.out.println(response.toString());
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            // Se actualiza la tabla con la nueva lista de platillos
-            tablaPlatillos.setItems(platos);
-            // Se imprime la información del platillo agregado en la consola
-            System.out.println(platillo.toString());
+            return;
         }
-    }
 
+        // Si todos los campos de texto tienen valores, se crea un objeto Platillos con esos valores y se agrega
+    }
 
     @FXML
     private void modificarPlatillo(ActionEvent event) {
-        // Función que se implementará para permitir la modificación de un platillo existente en la lista de platillos
+        // Se obtiene el objeto Platillos seleccionado en la tabla
+        Platillos platillo = tablaPlatillos.getSelectionModel().getSelectedItem();
+        if (platillo == null) {
+            // Si no se ha seleccionado ningún objeto Platillos, se muestra una alerta de error
+            AlertW.display("Error", "Por favor seleccione un platillo de la tabla");
+        } else {
+            // Si se ha seleccionado un objeto Platillos, se muestra una ventana para modificar los valores de sus propiedades
+            TextInputDialog dialog = new TextInputDialog();
+            dialog.setTitle("Modificar Platillo");
+            dialog.setHeaderText("Modificar Platillo");
+            dialog.setContentText("Ingrese los nuevos valores para el platillo:");
+
+            // Se establecen los valores actuales del objeto Platillos en los campos de texto de la ventana de diálogo
+            dialog.getEditor().setText(platillo.getNombrePlatillo() + "," + platillo.getCantCalorias() + "," + platillo.getTiempoPreparacion() + "," + platillo.getPrecio());
+
+            // Se muestra la ventana de diálogo y se obtiene la respuesta del usuario
+            Optional<String> resultado = dialog.showAndWait();
+
+            if (resultado.isPresent()) {
+                // Si el usuario ingresó una respuesta, se separa la cadena en sus componentes y se modifica el objeto Platillos
+                String[] valores = resultado.get().split(",");
+                platillo.setNombrePlatillo(valores[0]);
+                platillo.setCantCalorias(Integer.parseInt(valores[1]));
+                platillo.setTiempoPreparacion(Integer.parseInt(valores[2]));
+                platillo.setPrecio(Integer.parseInt(valores[3]));
+                // Se actualiza la tabla para reflejar los cambios
+                tablaPlatillos.refresh();
+            }
+        }
     }
 
     @FXML
     private void eliminarPlatillo(ActionEvent event) {
-        // Función que se implementará para permitir la eliminación de un platillo existente en la lista de platillos
+        // Se obtiene el objeto Platillos seleccionado en la tabla
+        Platillos platillo = tablaPlatillos.getSelectionModel().getSelectedItem();
+        if (platillo == null) {
+            // Si no se ha seleccionado ningún objeto Platillos, se muestra una alerta de error
+            AlertW.display("Error", "Por favor seleccione un platillo de la tabla");
+        } else {
+            // Si se ha seleccionado un objeto Platillos, se muestra una ventana de confirmación antes de eliminarlo
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Eliminar Platillo");
+            alert.setHeaderText("Eliminar Platillo");
+            alert.setContentText("¿Está seguro de que desea eliminar el platillo seleccionado?");
+
+            // Se muestra la ventana de confirmación y se obtiene la respuesta del usuario
+            Optional<ButtonType> resultado = alert.showAndWait();
+
+            if (resultado.get() == ButtonType.OK) {
+                // Si el usuario confirmó la eliminación, se elimina el objeto Platillos de la lista observable de platillos
+                platos.remove(platillo);
+            }
+        }
     }
 }
